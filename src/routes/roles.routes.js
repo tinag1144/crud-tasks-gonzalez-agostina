4da6ff6ac2ab56abdfa301c65bd3b4052db248f9
unknown <ag444107@gmail.com>
2025-08-28 23:42:15 -0300

    Faltaron las rutas de roles

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -1,9 +1,14 @@
+import { validator } from "../middlewares/validator.js";
 import { Router } from "express";
-import { createRol, getAllRoles } from "../controllers/roles.controllers.js";
+import { createRol, deleteRol, getAllRoles, getRolById, updateRol } from "../controllers/roles.controllers.js";
+import { createRolValidation, deleteRolValidation, getRolValidation, updateRolValidation } from "../middlewares/validations/roles.validations.js";
 
 const router = Router();
 
-router.post("/create-rol", createRol);
-router.get("/roles", getAllRoles);
+router.post("/rol", createRolValidation, validator, createRol);
+router.get("/rols", getAllRoles);
+router.get("/rol/:id", getRolValidation, validator, getRolById);
+router.put("/rol/:id", updateRolValidation, validator, updateRol);
+router.delete("/rol/:id", deleteRolValidation, validator, deleteRol)
 
 export default router;
